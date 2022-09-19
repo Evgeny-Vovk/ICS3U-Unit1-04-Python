d17f7669010aa491b4d89064108cd0e1b9805f7c
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-19 15:26:49 +0000

    done with python

---
+++ ICS3U-Unit1-04.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved
+#
+# Created by: Evgeny Vovk
+# Created on: September 2022
+# ICS3U-Unit1-03.py File, learning simple math in python
+
+
+def main():
+
+    print("The width of a square is 5 cm and the length is 9 cm.")
+
+    print("The area of the square is {} cm².".format(5 * 9))
+
+    print("The perimeter of the square is {} cm.".format(2 * (5 + 9)))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
